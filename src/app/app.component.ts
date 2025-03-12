4c1cdc60501affe0eb945c03edc772199af53f87
akishajae <akishajae.angeles@gmail.com>
2025-03-12 17:17:38 +0100

    in progress: room card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomCardComponent } from './room-card/room-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RoomCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/room-card/room-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-card',
+  imports: [],
+  templateUrl: './room-card.component.html',
+  styleUrl: './room-card.component.css'
+})
+export class RoomCardComponent {
+
+}
