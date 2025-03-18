6f9b787ade9ff3afc78dc84b1ddc25e896711920
ColoSans1 <arnaucolominassans2004@gmail.com>
2025-03-18 16:46:52 +0100

    CrateCommpoent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UserAlertIconsComponent } from  './user-alert-icons/user-alert-icons.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, UserAlertIconsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
