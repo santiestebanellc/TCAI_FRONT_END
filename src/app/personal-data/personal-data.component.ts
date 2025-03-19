7c631d747550af314b4182ddf01188fa12b41d1b
ColoSans1 <arnaucolominassans2004@gmail.com>
2025-03-19 16:32:26 +0100

    ChangeUpdateFirst

--- src/app/personal-data/personal-data.component.ts
+++ src/app/personal-data/personal-data.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-personal-data',
-  imports: [],
   templateUrl: './personal-data.component.html',
-  styleUrl: './personal-data.component.css'
 })
-export class PersonalDataComponent {
+export class PersonalDataComponent implements OnInit {
+  personalData: any = {};
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+    this.personalData = {
+      nom: 'Joan',
+      cognoms: 'Martínez Pérez',
+      dataNaixement: '1990-05-15',
+      adreca: 'Carrer Major, 123, Barcelona',
+      alergies: 'Alergia a los frutos secos y al polen',
+      antecedentsMedics: 'Hipertensión diagnosticada en 2018, asma leve, cirugía de apendicitis en 2010',
+      cuidador: 'Maria Gómez',
+      telefon: '612 345 678',
+    };
+  }
 }
