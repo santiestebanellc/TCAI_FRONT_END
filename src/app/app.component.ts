9cfb9a9fd25e540a514fb516ae10fbcb849e0201
ColoSans1 <arnaucolominassans2004@gmail.com>
2025-03-21 16:13:32 +0100

    #14CreateCompoentButton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PersonalDataComponent } from '../app/personal-data/personal-data.component';
 import { MedicalDataDisplayComponent } from './medical-data-display/medical-data-display.component'; 
+import { ButtonComponent } from './button/button.component';
+import { ButtonIconComponent } from './button-icon/button-icon.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MedicalDataDisplayComponent],
+  imports: [RouterOutlet, ButtonIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
