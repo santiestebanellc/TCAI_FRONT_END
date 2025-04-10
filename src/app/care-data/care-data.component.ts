39bdc542eab6fb7aeff118a2e7434687d7d3ca33
ColoSans1 <arnaucolominassans2004@gmail.com>
2025-04-10 16:00:36 +0200

    #34ChangeUpdate

--- src/app/care-data/care-data.component.ts
+++ src/app/care-data/care-data.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CareDataSummaryComponent } from '../care-data-summary/care-data-summary.component';
+import { HistoricalComponent } from '../historical/historical.component';
 
 @Component({
   selector: 'app-care-data',
-  imports: [CommonModule],
+  imports: [CommonModule, CareDataSummaryComponent, HistoricalComponent ],
   templateUrl: './care-data.component.html',
   styleUrl: './care-data.component.css',
 })
