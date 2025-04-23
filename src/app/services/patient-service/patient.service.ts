83775596cafbaad3570ba7a4e690f7173549a4cb
ColoSans1 <arnaucolominassans2004@gmail.com>
2025-04-23 15:23:23 +0200

    #45ChangeUpdate

--- src/app/services/patient-service/patient.service.ts
+++ src/app/services/patient-service/patient.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
+  private apiUrl = 'http://localhost:8000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getRegistrosByPaciente(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/registro/paciente/${id}`);
+  }
 }
