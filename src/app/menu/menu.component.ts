77949b84c5e52992a376cfc59d9fbdd0f953ba6f
akishajae <akishajae.angeles@gmail.com>
2025-04-24 17:07:01 +0200

    in progress: add nav icons

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PatientDataNavIconsComponent } from "../patient-data-nav-icons/patient-data-nav-icons.component";
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterModule],
+  imports: [RouterModule, PatientDataNavIconsComponent],
   standalone: true,
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
