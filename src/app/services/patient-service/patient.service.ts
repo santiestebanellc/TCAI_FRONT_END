1ab439ceb3a56c99932962ed4c6b531a39f38ee9
ColoSans1 <arnaucolominassans2004@gmail.com>
2025-04-30 11:40:16 +0200

    SummaryDataService #45

--- src/app/services/patient-service/patient.service.ts
+++ src/app/services/patient-service/patient.service.ts
@@ -1,22 +1,31 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
-  private apiUrl = 'http://localhost:8000';
+  private apiUrl = 'http://localhost:8000'; 
 
   constructor(private http: HttpClient) {}
 
-  // 🩺 Obtener registros por paciente
-  getRegistrosByPaciente(id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/registro/paciente/${id}`);
+  // 🩺 Obtener datos del paciente
+  getPatientData(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/registro/${id}`);
   }
 
   // 🧠 Obtener historial (diagnósticos) por paciente
   getDiagnosticoByPaciente(id: number): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/diagnostico/paciente/${id}`);
   }
+
+  // 🏥 Obtener datos generales de atención del paciente
+  getCareDataByPaciente(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/registro/${id}`);
+  }
+  
 }
+
+
